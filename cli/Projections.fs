5ba46dcb53778a0e933e2e930c755ee545e78645
Ruben Bartelink <ruben@bartelink.com>
2020-02-09 10:45:57 +0000

    Add second variant of E1

--- cli/Projections.fs
+++ cli/Projections.fs
@@ -1,11 +1,25 @@
 ﻿module Projections
 
+(* E1: Simplest answer *)
+
 let projectCount (xs : Event seq) : unit =
     let count = xs |> Seq.length
     printfn "Count: %d" count
 
+(* E1: Approaching it as a fold *)
+
+let evolveCount count (x : Event) =
+    count + 1
+
+let projectCountViaFold xs =
+    let res = (0, xs) ||> Seq.fold evolveCount
+    printfn "Count: %d" res
+
+(* TODO: the rest! *)
+
 let projection2 (xs : Event seq) : unit =
     // START HERE!
+    ()
 
 let run (xs : Event seq) =
     // While there are plenty ways to chain the sequence through each projection in a single pass
@@ -13,5 +27,9 @@ let run (xs : Event seq) =
     // and lets each implementation work based on query comprehensions, Seq expressions and/or even simple iteration/looping
     let xs = Seq.cache xs
 
+    // EXERCISE 1: Count the events
     xs |> projectCount
+    xs |> projectCountViaFold
+
+    // Other exercises can be slotted in here - in practice for a real system, you'd likely feed items one by one into each evolve function
     xs |> projection2
