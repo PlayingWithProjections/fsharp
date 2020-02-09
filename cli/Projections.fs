4414f4c97fc2bd63bc743080552970ea86c38eac
Ruben Bartelink <ruben@bartelink.com>
2020-02-09 10:36:42 +0000

    Move projectiona out to individual functions

--- cli/Projections.fs
+++ cli/Projections.fs
@@ -1,9 +1,17 @@
 ﻿module Projections
 
+let projectCount (xs : Event seq) : unit =
+    let count = xs |> Seq.length
+    printfn "Count: %d" count
+
+let projection2 (xs : Event seq) : unit =
+    // START HERE!
+
 let run (xs : Event seq) =
-    // While there are plenty ways to chain the sequence through each projection in a single pass, this lets one express
-    // the derivations as pipeline expressions
+    // While there are plenty ways to chain the sequence through each projection in a single pass
+    // using Seq.cache to hold them in memory after the first pass works for the datasets this workshop presents
+    // and lets each implementation work based on query comprehensions, Seq expressions and/or even simple iteration/looping
     let xs = Seq.cache xs
 
-    let count = Seq.length xs
-    printfn "Count: %d" count
+    xs |> projectCount
+    xs |> projection2
